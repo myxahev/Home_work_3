77280b2f62561db04d7ee6d29b09f5c686ff15bb
Dmitry <myxahev2@gmail.com>
2021-01-21 19:00:17 +0500

    complete 3.4

--- 3.3.py
+++ 3.3.py
@@ -1,4 +1,4 @@
-def my_func(a, b, c):
+def min_list(a, b, c):
     my_list = [a, b, c]
     my_list.remove(min(my_list))
     print('Сумма наибольших двух аргументов равна:', sum(my_list))
@@ -6,4 +6,4 @@ def my_func(a, b, c):
 user_num1 = int(input("Введите первое число: "))
 user_num2 = int(input("Введите второе число: "))
 user_num3 = int(input("Введите третье число: "))
-my_func(user_num1, user_num2, user_num3)
+min_list(user_num1, user_num2, user_num3)
